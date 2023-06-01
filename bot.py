af2a70908830b2c7b510a65c372583dc7d4589a0
yashwant chauhan <131139103+dev-Yashwant@users.noreply.github.com>
2023-06-01 17:52:46 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import random
+import telebot
+
+Token = ""
+
+bot = telebot.TeleBot(Token)
+
+@bot.message_handler(['quote'])
+def start(msg):
+    try:
+        with open('quotes.txt', 'r') as file:
+            lines = file.readlines()
+            random_line = random.choice(lines)
+
+        bot.reply_to(msg,random_line)
+    except Exception as e:
+        print(e)
+        bot.reply_to(msg, "Oops!server is busy try again ")
+
+bot.polling()
